eae123f8c61b893900c86d036a70ebabf2f3f01b
hmadzlnii <hmadzlnii@gmail.com>
2024-11-12 20:43:03 +0700

    Ahmad | add helper for clean CLI

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # import src.design.login
-import src.design.test as testDesign
-import src.design.dashboard_spv as dashboardSpv
-dashboardSpv.dashboard
+#import src.design.test as testDesign
+#import src.design.dashboard_spv as dashboardSpv
+#dashboardSpv.dashboard
 import src.design.login as login
 
 login.login()
 # testDesign.getAll()
 # testDesign.queryTest()
 
-testDesign.getAll()
-testDesign.queryTest()
-testDesign.createEVent()
+#testDesign.getAll()
+#testDesign.queryTest()
+#testDesign.createEVent()
--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,10 @@
+import os
+
 def generateTitle(nama, length):
     return print(f"{'=' * length} {nama} {'=' * length}")
 
+def clear():
+  if os.name == 'nt':  # Windows
+    os.system("cls")
+  else:
+    os.system("clear")
