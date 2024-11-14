f63eb5ad65675792645c4e9eec5c03147ac53c16
tehbotoloishii <f0r13xo@gmail.com>
2024-11-14 08:03:38 +0700

    Malyhah | logout

--- src/design/dashboard_user.py
+++ src/design/dashboard_user.py
@@ -1,9 +1,10 @@
 from utils.helper import generateTitle, clear
 import inquirer
 
+
 def dashboard():
     clear()
-    print(generateTitle("Dashboard", 14))
+    print(generateTitle("Dashboard User", 14))
     print("Welcome to the user's dashboard!!")
 
     answer = inquirer.list_input("Go to...", choices=["List All Assigned Event", "List All Event", "Logout"])
@@ -15,7 +16,17 @@ def dashboard():
         print("hal all event")
         # list_all_created_event() #hal list all created event
     elif answer == "Logout":
-        print("logout")# logout()
+        from src.design.first_page import first_page
+        while True:
+                confirm = input("Konfirmasi (yes/no): ").lower().strip()
+                if confirm in ["yes", "y"]:
+                    first_page()
+                    break
+                elif confirm in ["no", "n"]:
+                    break
+                else:
+                    print("Invalid input. Please enter 'yes' or 'no'.")
+     
     else:
         print("Invalid choice. Please choose a valid option.")
         dashboard()
