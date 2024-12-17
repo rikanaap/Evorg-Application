66637ad7d963ea1d16c9297bef832f9002cf026b
hmadzlnii <hmadzlnii@gmail.com>
2024-12-17 07:33:33 +0700

    feat: Create rundown datas to .json

--- src/design/createRundown.py
+++ src/design/createRundown.py
@@ -4,6 +4,7 @@ from utils.local import getLocalRundown, getLocalRundownId
 from src.service.roundown import Roundown
 from src.design.confirm_rundown import confirmRundown
 
+_rundownService = Roundown()
 
 def createRundown(callback, index_below):
     rundownId = getLocalRundownId()
@@ -11,18 +12,32 @@ def createRundown(callback, index_below):
 
     clear(), generateTitle('Add New Rundown', 14)
     rundown_name = input("Rundown Name\t: ")    
-    description = multilineInput("Description\t: ")
-    duration = int(input("Duration\t: "))
-    index =  tableInputRundown(createRundown, event_data.get("roundown_identifier"))
+    description = input("Description\t: ")
+    try:
+      duration = int(input("Duration (menit)\t: "))
+    except ValueError:
+      print("Durasi harus berupa angka!")
+      return callback()
+    index_below =  tableInputRundown(createRundown, rundownId)
 
-    # confirmRundown("create"
-    # , #TODO: Function when success or confirmation is yes
-    # , #TODO: Function when failed or confirmation is no
-    # , data={
-    #     "roundown_name": rundown_name, 
-    #     "duration": duration, 
-    #     "description": description, 
-    #     "index_below": index_below
-    #     })
+    new_rundown = {
+        "roundown_name": rundown_name, 
+        "duration": duration, 
+        "description": description,
+        "index_below": index_below
+    }
 
+    def on_create_success():
+      result = _rundownService.createOne(rundownId, new_rundown)
+      if result:
+        print("Rundown successfully created!")
+      else:
+        print("Failed to create rundown.")
+        
+    def on_create_failure():
+      print("Rundown creation cancelled!")
 
+    confirmRundown("create"
+     , on_create_success()
+     , on_create_failure
+     , data=new_rundown)
