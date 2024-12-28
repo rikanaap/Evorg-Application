4bcf9358d6704d917e613095f19464d957445469
tehbotoloishii <f0r13xo@gmail.com>
2024-12-28 23:04:46 +0700

    Malyhah | fix bug register

--- src/design/register.py
+++ src/design/register.py
@@ -9,39 +9,47 @@ def register_participant():
     userService = User()
     generateTitle("Registrasi Peserta", 14)
 
-    username = input("Masukkan Username\t: ")
+    while True:
+        username = input("Masukkan Username\t: ")
+        if not username:
+            print("Username tidak boleh kosong")
+            continue
 
-    # Cek apakah username sudah terdaftar di user.json
-    check = userService.cekRegister(username)
-    if not check["success"]:
-        print(check["message"])
-        time.sleep(1)
-        return register_participant()
-
-    password = input("Masukkan Password\t: ")
-    enkripsi = userService.hashPassword(password)
-    role = input("Pilih Role (spv/user)\t: ").lower()
-    
-    #validasi inputan role
-    while role not in ["spv", "user"]:
-        role = input("Role tidak valid. Masukkan 'spv' atau 'user'\t: ").lower()
-
-    confirm = input("Apakah Anda yakin ingin mendaftar? (yes/no): ").lower()
-    if confirm == "yes":
-        data = {
-            "username": username,
-            "password": enkripsi,
-            "role": role,
-        }
-        result = userService.createOne(data)
-        if result:
-            print("Registrasi berhasil.")
+        # Cek apakah username sudah terdaftar di user.json
+        check = userService.cekRegister(username)
+        if not check["success"]:
+            print(check["message"])
             time.sleep(1)
-            login()
-        else:
-            print("Terjadi kesalahan saat menyimpan data.")
-    else:
-        print("Registrasi dibatalkan.")
-    time.sleep(1)
+            return register_participant()
+        
+        while True:
+            password = input("Masukkan Password\t: ")
+            if not password:
+                print("Password tidak boleh kosong")
+                continue
+            enkripsi = userService.hashPassword(password)
+            role = input("Pilih Role (spv/user)\t: ").lower()
+            
+            #validasi inputan role
+            while role not in ["spv", "user"]:
+                role = input("Role tidak valid. Masukkan 'spv' atau 'user'\t: ").lower()
+
+            confirm = input("Apakah Anda yakin ingin mendaftar? (yes/no): ").lower()
+            if confirm == "yes":
+                data = {
+                    "username": username,
+                    "password": enkripsi,
+                    "role": role,
+                }
+                result = userService.createOne(data)
+                if result:
+                    print("Registrasi berhasil.")
+                    time.sleep(1)
+                    login()
+                else:
+                    print("Terjadi kesalahan saat menyimpan data.")
+            else:
+                print("Registrasi dibatalkan.")
+            time.sleep(1)
 
 
